6d375d03338884ffe0e14f63d5a5ab1b563a7f2d
Kassandra Perch <the@nodebotani.st>
2018-09-09 13:13:15 -0500

    Added mqtt lib

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const eventEmitter = require('events').EventEmitter;
 const util = require('util');
 const mqtt = require('mqtt');
+let client = null;
 
 const Receiver = function(options) {
   const AIOUsername = options.AIOUsername;
   const AIOkey = options.AIOkey;
+  client = mqtt.connect('mqtt://io.adafruit.com', {
+    username: AIOUsername,
+    password: AIOkey
+  })
   eventEmitter.call(this);
 }
 
