fd4ae74d18f27ae28c087fb77af0745b36599ab1
Kassandra Perch <the@nodebotani.st>
2018-09-09 13:44:28 -0500

    Adding a console.log debug statement because who has not do not judge

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const mqtt = require('mqtt');
 let client = null;
 
 const Receiver = function(options) {
+  console.log('In the AdafruitIO receiver!')
   const AIOUsername = options.AIOUsername;
   const AIOkey = options.AIOkey;
   client = mqtt.connect('mqtt://io.adafruit.com', {
     username: AIOUsername,
     password: AIOkey
   })
+
+  client.on('connect', () => {
+    console.log('connected to AdafruitIO')
+  })
+
+  client.on('error', (err) => {
+    console.log('Error connecting to AdafruitIO: ', err)
+  })
   eventEmitter.call(this);
 }
+
 
 util.inherits(Receiver, eventEmitter);
 
